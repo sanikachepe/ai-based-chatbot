1334408793d6586a1186462fc7bd04cead18ec90
anonymousb61 <64683559+anonymousb61@users.noreply.github.com>
2021-06-24 23:48:02 +0530

    Add files via upload

--- actions.py
+++ actions.py
@@ -14,6 +14,7 @@ from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
 from rasa_sdk.forms import FormAction
 from rasa_sdk.events import FollowupAction
+from rasa_sdk.events import AllSlotsReset
 #from rasa.core.trackers import DialogueStateTracker
 
 # class ActionHelloWorld(Action):
@@ -37,14 +38,15 @@ class ActionFacilitySearch(Action):
    def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        entities = tracker.latest_message['entities']
+
+        location = tracker.get_slot("location")
         dispatcher.utter_message(text="Facility Search Action!")
 
-        return []
+        return [AllSlotsReset()]
 
 
 
-class ActionHelloWorld(Action):
+class ActionDrivingLicense(Action):
 
     def name(self) -> Text:
          return "action_driving_license_form"
@@ -55,28 +57,89 @@ class ActionHelloWorld(Action):
          age = tracker.get_slot("age")    
          location = tracker.get_slot("location")
          int_age = int(age)
-         flag = False 
+         #flag_ll = False 
+         flag_dl = False
 
          message ="" 
          if int_age < 16:
            message = "sorry, you aren't eligible for license "
          elif int_age >=16 and int_age <18    :
-           message= "you can apply for lerner’s licence for Motorcycles without gear with capacity up to 50cc and require parent’s or guardian’s permission "
+           message= "you can apply for learner’s licence for Motorcycles without gear with capacity up to 50cc and require parent’s or guardian’s permission "
          else :
            message = "you are eligible for license "
-           flag = True
+           flag_dl = True
+
+         final_message = message + "in " + location  #check location and age
+         
+         dispatcher.utter_message(text= final_message)
+
+         if flag_dl == True:
+           
+             dispatcher.utter_message(text = """How to submit your driving licence application form
+                          • Visit the Sarathi website of the Ministry of Road Transport and Highways.
+                          • On your left hand side, you will see options under “Driving Licence”.
+                          • Choose “Apply Online” option and from the drop down choose “New Driving Licence”.
+                          • You will now see instructions for submitting your driving licence application form. Please read the instructions carefully and click on the “Continue” button to proceed.
+                          • Once you click on the “Continue” button, will be asked if you are holding a Learner’s Licence, a Foreign DL, or a Defence Licence. Tick the appropriate box and proceed.
+                          • Based on what you selected above, you will be asked for the Learner’s Licence Number/Foreign DL NUmber/ Defence Licence Number.
+                          • You will also be asked to enter your date of birth in DDMMYYYY format.
+                          • Once you fill the information asked, please click on “OK”.
+                          • In this form, you will be asked to fill up your personal details like your name, age, and address.
+                          • Once you click on all the details, you will be asked to upload all the required supporting documents like address proof, age proof, and identity proof.
+                          • You may be asked to upload a passport size photograph and your signature. Please keep the scanned copies for the same ready.
+                          • Once you have uploaded all the documents, you will be asked to choose the timing for your DL appointment. Please select the date and the time when you will be available to go directly to the RTO and appear for your DL test.
+                          • Once you make the payment to submit your driving licence application form, your application will be successfully sent to the RTO.
+                          • On your appointment day, make sure you reach few minutes early and give your test.
+                      The section is to be signed in the presence of the Licensing Authority by the parent/guardian.
+                      On completion of the form and after reviewing the information, the applicant can submit the form online by clicking on the 'Submit' button at the end of the form.
+                      On submission, an auto-generated Web Application Number will appear on screen. Note the number for future reference and to check the status of the application.""")           
+             #FollowupAction("action_followup_check")
+             flag_dl = False
+
+         return [AllSlotsReset()]
+
+
+class ActionLearnersLicense(Action):
+
+    def name(self) -> Text:
+         return "action_learners_license_form"
+
+    def run(self, dispatcher: CollectingDispatcher,
+             tracker: Tracker,
+             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+         age = tracker.get_slot("age")    
+         location = tracker.get_slot("location")
+         int_age = int(age)
+         flag_ll = False 
+         
+
+         message ="" 
+         if int_age < 16:
+           message = "sorry, you aren't eligible for license "
+         elif int_age >=16 and int_age <18    :
+           message= "you can apply for learner’s licence for Motorcycles without gear with capacity up to 50cc and require parent’s or guardian’s permission "
+           flag_ll = True
+         else :
+           message = "you are eligible for learners license "
+           flag_ll = True
 
          final_message = message + "in " + location  
          
         # print(final_message) 
          dispatcher.utter_message(text= final_message)
 
-         if flag == True:
-             dispatcher.utter_message("You need some documents:") 
-             FollowupAction("action_followup_check")
-             flag = False
+         if flag_ll == True:
+             dispatcher.utter_message(text ="""Steps to get Learner’s licence: 
+                         
+                        1) you need to register on your district’s RTO website by filling the application forms
+                        2) visit the nearest RTO to submit the application
+                        3) register on parivahan.gov.in for test
+                        4) visit RT office for test
+                        5) On passing the test, the learner’s licence will be sent to your permanent address.
+                         """) 
+             flag_ll = False
 
-         return []
+         return [AllSlotsReset()]
 
 
 class ActionFollowCheck(Action):
